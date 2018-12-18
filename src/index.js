0daa4f39e6a1790f15dc83d1b65903fa7d9b1b39
Bruce Thomas <response.write@gmail.com>
2018-12-18 23:48:59 +0000

    refactored events to Component

--- src/index.js
+++ src/index.js
@@ -17,25 +17,8 @@ window.RE = window.RE || {};
 
 const localState = u.storage();
 const state = localState.read() || initialState;
-// localState.write(state);
 
 const store = createStore(combineReducers(allReducers), state);
-
-// const tiggerDict = {
-//   ALTenter: e => {
-//     const el = window.getSelection().focusNode.parentNode;
-//     const { id } = el;
-
-//     el.classList.add("locked");
-
-//     const text = u.inflate(el.innerText);
-
-//     store.dispatch({ type: "CONTENT-LOCK", id: id });
-//     store.dispatch({ type: "EDITOR-LOAD", id: id, text: text });
-
-//     return false;
-//   }
-// };
 
 const App = () => {
   return (
@@ -48,51 +31,3 @@ const App = () => {
 
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
-
-function updateIO(value = null) {
-  const el = document.getElementById("io");
-  el.value = value;
-  el.focus();
-}
-
-document.querySelector("article").onclick = e => {
-  const { id } = e.target;
-  const { editor } = store.getState();
-
-  // if (id && state.locked[id]) editor(id);
-  if (editor[id]) {
-    store.dispatch({ type: "EDITOR-LOAD", id: id, text: null });
-    updateIO(editor[id]);
-  }
-};
-
-document.querySelector("article").ondblclick = e => {
-  const el = e.target;
-  el.id = el.id || u.uuid();
-
-  const { id } = el;
-  const { editor } = store.getState();
-
-  if (editor[id] && window.confirm("Are you sure?")) {
-    store.dispatch({ type: "EDITOR-DELETE", id: id });
-    store.dispatch({ type: "CONTENT-UNLOCK", id: id });
-    el.classList.remove("locked");
-    console.log(3, "render the candidate", store.getState());
-    updateIO();
-    return;
-  }
-
-  if (!editor[id]) {
-    store.dispatch({
-      type: "EDITOR-LOAD",
-      id: id,
-      text: u.inflate(el.innerText)
-    });
-    store.dispatch({ type: "CONTENT-LOCK", id: id });
-    el.classList.add("locked");
-
-    console.log(3, "lock the element", store.getState());
-    updateIO(editor[id]);
-    return;
-  }
-};
