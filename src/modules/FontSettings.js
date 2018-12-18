75f7b9911b0a147a19bade04c59f3a02e17ea6ae
Bruce Thomas <response.write@gmail.com>
2018-12-18 23:50:31 +0000

    refactor font setting controls

--- src/modules/FontSettings.js
+++ src/modules/FontSettings.js
@@ -1,8 +1,11 @@
 import React from "react";
+import u from "../utilities/";
+
+let store;
 let fface = 0;
 let fsize = 1;
 
-function fontFamily(modifier = 0, size = 1) {
+function setFontFamily(modifier = 0, size = 1, noSave) {
   const body = document.querySelector("body");
   const modifiers = ["serif", "sanserif", "monospace"];
   const sizes = [14, 18, 24, 26];
@@ -17,21 +20,28 @@ function fontFamily(modifier = 0, size = 1) {
 
   fface = modifier;
   fsize = size;
+
+  if (noSave) return;
+
+  store.dispatch({ type: "font-family", value: fface });
+  store.dispatch({ type: "font-size", value: fsize });
+
+  u.storage().write(store.getState());
 }
 
 const FontSizes = () => {
   return (
     <ul>
-      <a href="#0" onClick={() => fontFamily(fface, 0)}>
+      <a href="#0" onClick={() => setFontFamily(fface, 0)}>
         14
       </a>
-      <a href="#1" onClick={() => fontFamily(fface, 1)}>
+      <a href="#1" onClick={() => setFontFamily(fface, 1)}>
         18
       </a>
-      <a href="#2" onClick={() => fontFamily(fface, 2)}>
+      <a href="#2" onClick={() => setFontFamily(fface, 2)}>
         24
       </a>
-      <a href="#3" onClick={() => fontFamily(fface, 3)}>
+      <a href="#3" onClick={() => setFontFamily(fface, 3)}>
         26
       </a>
     </ul>
@@ -40,20 +50,23 @@ const FontSizes = () => {
 const FontFaces = () => {
   return (
     <ul>
-      <a href="#0" onClick={() => fontFamily(0)}>
+      <a href="#0" onClick={() => setFontFamily(0)}>
         Serif
       </a>
-      <a href="#1" onClick={() => fontFamily(1)}>
+      <a href="#1" onClick={() => setFontFamily(1)}>
         Sans
       </a>
-      <a href="#2" onClick={() => fontFamily(2)}>
+      <a href="#2" onClick={() => setFontFamily(2)}>
         Mono
       </a>
     </ul>
   );
 };
 
-export default () => {
+export default props => {
+  store = store || props.store;
+  const { fontFamily, fontSize } = store.getState().settings;
+  setFontFamily(fontFamily, fontSize, true);
   return (
     <div>
       <FontFaces />
