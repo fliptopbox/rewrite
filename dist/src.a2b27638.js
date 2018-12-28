38523e8d37c324473dc2eb92d253b54e603c047c
Bruce Thomas <response.write@gmail.com>
2018-12-28 23:42:48 +0000

    remove heading tags

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -1478,7 +1478,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36347" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38237" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
