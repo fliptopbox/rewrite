8ec2ebcad5fb39d637723f4dd382325609fb421e
Bruce Thomas <response.write@gmail.com>
2019-01-19 19:06:05 +0000

    new UUID algo

--- src/utilities/uuid.js
+++ src/utilities/uuid.js
@@ -1,15 +1,38 @@
-let serial = -1;
-const uuid = () => {
-  serial++;
-  return (
-    String.fromCharCode(65 + (serial % 26)).toLowerCase() +
-    (
-      new Date().valueOf().toString(16) + // eg 127b795136 (11)
-      Math.floor(1000 + Math.random() * 1000).toString(13) + // eg aa7 (3)
-      (1000 + (serial % 1000)).toString(5) + // eg 13001 (5)
-      ""
-    ).slice(-15)
-  );
-};
+let snA = 0;
+let snB = 0;
 
+// fractionally slow runtime
+// --------------------------
+// function v1() {
+//   const a = (((snA += 1) % 6) + 10).toString(16);
+//   const b = new Date().valueOf().toString(16);
+//   const c = 1000 + ((snB += 1) % 999);
+//   return `${a}${b}${c}`;
+// }
+
+function v2() {
+  // alpha + timestamp + 1000 (1+11+4=16)
+  return (
+    (((snA += 1) % 6) + 10).toString(16) +
+    new Date().valueOf().toString(16) +
+    (1000 + ((snB += 1) % 999)) +
+    ""
+  );
+}
+
+const uuid = v2;
 export default uuid;
+
+/** QUOKKA INLINE UNIT TEST  * /
+
+var max = 10000000;
+console.time("v");
+for(var i = 0; i < max; i ++) v1();
+console.timeEnd("v");
+
+console.time("v");
+for(var i = 0; i < max; i ++) v2();
+console.timeEnd("v");
+console.log(new Date())
+
+/** */
--- src/utilities/uuid.test.js
+++ src/utilities/uuid.test.js
@@ -1,13 +1,15 @@
-import u from "./index";
+import uuid from "./uuid";
 
-test("uuid", () => {
+test("uuid generates 100k unique strings 16 chars length", () => {
   const dict = {};
-  let i = 100000;
+  let i = 1000000;
   let x = i;
-  while (i--) {
-    dict[u.uuid()] = true;
-  }
+  while (i--) dict[uuid()] = true;
+
   const keys = Object.keys(dict);
   expect(keys.length).toBe(x);
-  expect(keys.some(id => id.length !== 16)).toBe(false);
+
+  // must start alpha, then hex characters, total 16
+  const hex16 = /^[a-z][0-9a-z]{15}$/;
+  expect(keys.some(id => !hex16.test(id))).toBe(false);
 });
