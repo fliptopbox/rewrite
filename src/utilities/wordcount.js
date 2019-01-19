de04184b55b21a1ca215a62e18d62cf53619041a
Bruce Thomas <response.write@gmail.com>
2019-01-19 20:52:08 +0000

    wordcount update and unit test

--- src/utilities/wordcount.js
+++ src/utilities/wordcount.js
@@ -1,8 +1,15 @@
 function wordcount(value) {
-  return value
-    .replace(/[\.\n]+/g, "")
-    .replace(/(\s\s+)/g, "")
-    .split(" ").length;
+  value = String(value || "");
+
+  if (!value) return 0;
+
+  value = value
+    .replace(/\-{2,}/g, "")
+    .replace(/[\"\.\n]+/g, " ")
+    .replace(/(\s{1,})/g, " ")
+    .trim();
+
+  return value.length ? value.split(" ").length : 0;
 }
 
 export default wordcount;
