b6fae7196916d9f9240d25f9f71f76de5ffe7c67
Bruce Thomas <response.write@gmail.com>
2019-02-18 19:14:34 +0000

    moving to react controller

--- src/modules/Sidebar/index.js
+++ src/modules/Sidebar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Parse from "../../utilities/Parse";
 
 class Sidebar extends React.Component {
   constructor(props) {
     super();
     const { store } = props;
     this.state = {
-      articles: store.list()
+      articles: store.list(),
+      update: 0
     };
   }
 
@@ -31,10 +33,29 @@ class Sidebar extends React.Component {
   getFileRow(object) {
     const { guid, name, words = 1234, opened } = object;
     return (
-      <li key={guid}>
+      <li
+        key={guid}
+        onClick={() => {
+          const { store, article } = this.props;
+          const fileObj = store.read(guid);
+          const { data } = fileObj;
+
+          return article.init(data);
+        }}
+      >
         <span className="file-name" data-guid="${guid}">
-          <span contentEditable="true">{name}</span>
-          <a href="#">edit</a>
+          <input
+            type="text"
+            defaultValue={name}
+            onKeyPress={e => {
+              if (/enter/i.test(e.key)) {
+                console.log(e.target.value);
+                e.target.blur();
+                this.props.store.rename(guid, e.target.value);
+              }
+              return true;
+            }}
+          />
         </span>
         <div className="file-meta">
           <a href="#delete" onClick={() => this.handleDelete(guid)}>
@@ -50,8 +71,61 @@ class Sidebar extends React.Component {
       </li>
     );
   }
+  handleImport(e) {
+    const { store, article } = this.props;
+    const { setState } = this;
+    const update = new Date().valueOf();
+    store.open(e, function(name, text) {
+      const p = new Parse(text);
+      const current = store.create(null, name, p.toCollection());
+      article.init(current.data);
+    });
+    setTimeout(() => {
+      setState({ update: update });
+    }, 250);
+  }
+
   render() {
-    return this.getArticles();
+    const articleList = this.getArticles();
+    return (
+      <div>
+        <ul>
+          <li
+            onClick={() => {
+              this.props.store.create(null, "Untitled", [{}]);
+              this.props.article.init([{}]);
+              return;
+            }}
+          >
+            New
+          </li>
+          <li>
+            <label htmlFor="uploadInput">
+              <span>Open</span>
+              <input
+                id="uploadInput"
+                className="hidden"
+                onChange={this.handleImport.bind(this)}
+                type="file"
+                accept="text/*"
+              />
+            </label>
+          </li>
+          <li>Save</li>
+        </ul>
+        <hr />
+        {articleList}
+        <hr />
+        <ul>
+          <li>Toggle dark theme</li>
+          <li>Collapse lines</li>
+          <li>Strike through</li>
+          <li>Typewiter mode</li>
+          <li>Font size</li>
+          <li>Read paragraph</li>
+        </ul>
+      </div>
+    );
   }
 }
 
