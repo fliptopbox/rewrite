a64274f9345b0ab9452b6178aa2d116378f8de82
Bruce Thomas <response.write@gmail.com>
2019-02-23 02:17:20 +0000

    added MD switch. added modifiers, dataset attributes

--- src/utilities/collectionToHtml.js
+++ src/utilities/collectionToHtml.js
@@ -1,70 +1,97 @@
-import uuid from "./uuid";
-import getCandidateString from "./getCandidateString";
+import uuid from './uuid';
+import getCandidateString from './getCandidateString';
 
-import config from "../config";
+import config from '../config';
 export default v2;
 
 function v2(array, options) {
-  // returns HTML String
-  // Expected collection schema:
-  // [{selected: true, text: String, versions: Object}, ...]
+    // returns HTML String
+    // Expected collection schema:
+    // [{selected: true, text: String, versions: Object}, ...]
 
-  if (!array) return;
+    if (!array) return;
 
-  const { tag, br, re, flag } = { ...config.options, options };
-  let unique = true; // ensure only one selected DOM element
+    const { tag, br, re, flag } = { ...config.options, options };
+    let unique = true; // ensure only one selected DOM element
 
-  const html = array.map(row => {
-    let classnames = {};
+    const html = array.map(row => {
+        let classnames = {};
+        let modifiers = {};
+        let datasets = {};
 
-    const exclude = /^(text|versions|locked|selected)/i;
-    const { text = "", versions = "", selected = "" } = row;
-    const id = versions ? uuid() : "";
-    const candidate = versions && getCandidateString(versions);
-    const value = text || candidate || "";
-    const json = versions ? JSON.stringify(versions) : "";
+        const exclude = /^(text|versions|locked|selected)/i;
+        const { text = '', versions = '', selected = '' } = row;
+        const id = versions ? uuid() : '';
+        const candidate = versions && getCandidateString(versions);
+        const value = text || candidate || '';
+        const json = versions ? JSON.stringify(versions) : '';
 
-    const isEmpty = versions && !candidate ? true : false;
-    const isInactive = re.test(value) || row.inactive;
+        const isEmpty = versions && !candidate ? true : false;
+        const isInactive = re.test(value) || row.inactive;
 
-    // derived classNames
-    classnames.locked = versions ? true : null;
-    classnames[flag] = isInactive ? true : null;
-    classnames.selected = unique && selected ? true : null;
-    classnames.empty = isEmpty ? true : null;
+        // derived classNames
+        classnames.locked = versions ? true : null;
+        classnames[flag] = isInactive ? true : null;
+        classnames.selected = unique && selected ? true : null;
+        classnames.empty = isEmpty ? true : null;
 
-    // only add non-existant keys
-    Object.keys(row).forEach(key => {
-      if (exclude.test(key) || key in classnames) return;
-      classnames[key] = true;
+        // only add non-existant keys
+        Object.keys(row).forEach(key => {
+            if (exclude.test(key) || key in classnames) return;
+
+            // seperate modifiers
+            if (/^md?-/.test(key)) {
+                // Boolean values signify modifiers
+                if (/^(true)$/i.test(row[key])) {
+                    return (modifiers[key] = true);
+                }
+
+                // String values are datasets
+                return (datasets[key] = row[key]);
+            }
+
+            // append classnames
+            classnames[key] = true;
+        });
+
+        // flattern keys into string
+        classnames =
+            Object.keys(classnames)
+                .map(key => (classnames[key] ? key : ''))
+                .join(' ')
+                .trim() || null;
+
+        // ensure the "selected" element is unique
+        unique = selected ? false : unique;
+
+        const el = {};
+        el.id = id ? ` id="${id}"` : '';
+        el.className = classnames ? ` class="${classnames}"` : '';
+        el.dataset = versions ? ` data-versions='${json}'` : '';
+        el.innerHTML = value.replace(re, '') || br;
+
+        // catenate the modifiers
+        modifiers = Object.keys(modifiers).map(k => `${k}=""`);
+        modifiers = `${modifiers.join(' ')}`;
+        modifiers = modifiers.trim() ? ` ${modifiers}` : '';
+
+        // catenate datasets
+        datasets = Object.keys(datasets).map(
+            k => `data-${k}="${JSON.stringify(datasets[k])}"`
+        );
+        datasets = ` ${datasets.join(' ')}`;
+        datasets = datasets.trim() ? ` ${datasets}` : '';
+
+        return `<${tag}${el.id}${el.className}${modifiers}${datasets}${
+            el.dataset
+        }>${el.innerHTML}</${tag}>`;
+        // return el.outerHTML;
     });
 
-    // flattern keys into string
-    classnames =
-      Object.keys(classnames)
-        .map(key => (classnames[key] ? key : ""))
-        .join(" ")
-        .trim() || null;
+    // ensure there is always an extra last line
+    if (!/<br\s?\/?>/i.test(html.slice(-1))) {
+        html.push(`<${tag}>${br}</${tag}>`);
+    }
 
-    // ensure the "selected" element is unique
-    unique = selected ? false : unique;
-
-    const el = {};
-    el.id = id ? ` id="${id}"` : "";
-    el.className = classnames ? ` class="${classnames}"` : "";
-    el.dataset = versions ? ` data-versions='${json}'` : "";
-    el.innerHTML = value.replace(re, "") || br;
-
-    return `<${tag}${el.id}${el.className}${el.dataset}>${
-      el.innerHTML
-    }</${tag}>`;
-    // return el.outerHTML;
-  });
-
-  // ensure there is always an extra last line
-  if (!/<br\s?\/?>/i.test(html.slice(-1))) {
-    html.push(`<${tag}>${br}</${tag}>`);
-  }
-
-  return html.join("\n");
+    return html.join('\n');
 }
