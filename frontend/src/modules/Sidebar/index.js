4f32c546bf5bd4eb618c458edb4bf982ad25c696
Bruce Thomas <response.write@gmail.com>
2019-03-13 14:54:57 +0000

    move toggle input to external file

--- frontend/src/modules/Sidebar/index.js
+++ frontend/src/modules/Sidebar/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Parse from '../../utilities/Parse';
 import u from '../../utilities/';
+import ToggleToInput from './ToggleToInput';
+
 const { read, write, push } = u.storage('settings');
-
 const html = document.querySelector('html');
-
-// window.addEventListener("keyup", e => {
-//     if(!e.altKey) html.classList.remove("show-alternative");
-// });
 
 // a keypress toggle. 1st press = on, 2nd press = off
 // different platforms have strange quirks with holding down keys + clicking
@@ -20,73 +17,6 @@ window.addEventListener('keydown', e => {
     }
 });
 
-class ToggleToInput extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            edit: false,
-            name: props.name,
-            original: props.name,
-            guid: props.guid,
-        };
-    }
-    rename = (reset = false) => {
-        const { guid, name, original } = this.state;
-        let value = original;
-
-        if (!reset && name && name.trim() && name !== original) {
-            value = name.trim();
-            this.props.store.rename(guid, value);
-        }
-
-        this.setState({ edit: false, name: value, original: value });
-    };
-
-    toggleEditAndFocus = e => {
-        e.stopPropagation();
-        this.setState({ edit: true });
-        const { guid } = this.state;
-        setTimeout(() => document.querySelector(`#${guid}`).focus(), 150);
-    };
-
-    render() {
-        const { name, edit, guid } = this.state;
-        return !edit ? (
-            <span className="file-toggle-input">
-                {name}
-                <a href="#edit" onClick={this.toggleEditAndFocus}>
-                    edit
-                </a>
-            </span>
-        ) : (
-            <input
-                id={guid}
-                type="text"
-                tabIndex="0"
-                value={name}
-                onBlur={() => this.rename()}
-                onChange={e => {
-                    this.setState({ name: e.target.value });
-                }}
-                onClick={e => e.stopPropagation()}
-                onKeyDown={e => {
-                    let { key } = e;
-                    let reset = false;
-
-                    if (/^(escape)/i.test(key)) {
-                        key = 'enter';
-                        reset = true;
-                    }
-
-                    if (/^(enter)/i.test(key)) {
-                        this.rename(reset);
-                        e.target.blur();
-                    }
-                }}
-            />
-        );
-    }
-}
 class Sidebar extends React.Component {
     constructor(props) {
         super();
