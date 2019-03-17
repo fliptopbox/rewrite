7bbba357f7a58c19ed6aa98a2b2289c486e24e8e
Bruce Thomas <response.write@gmail.com>
2019-03-17 18:12:09 +0000

    excluded BE build assets

---
+++ backend/build/precache-manifest.12a75a209187ddb9abca965feede664f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "92bc245f63f61b50fbdc",
+    "url": "./static/js/main.92bc245f.chunk.js"
+  },
+  {
+    "revision": "6509bf6d299e687d8aed",
+    "url": "./static/js/2.6509bf6d.chunk.js"
+  },
+  {
+    "revision": "92bc245f63f61b50fbdc",
+    "url": "./static/css/main.c8ada68f.chunk.css"
+  },
+  {
+    "revision": "ca44064c388f09f94231a5f4f46fed42",
+    "url": "./index.html"
+  }
+];
